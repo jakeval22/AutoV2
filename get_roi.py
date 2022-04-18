8f64f7006726f21adb2a987b3a51e29f26e2bf48
jakeval22 <83443720+jakeval22@users.noreply.github.com>
2022-04-18 00:26:45 -0500

    Update get_roi.py

--- get_roi.py
+++ get_roi.py
@@ -1,30 +1,3 @@
-import cv2
-
-cap = cv2.VideoCapture(1) # video capture source camera (Here webcam of laptop)
-
-# reading the input using the camera
-result, image = cap.read()
-
-# If image will detected without any error,
-# show result
-if result:
-
-	# showing result, it take frame name and image
-	# output
-	cv2.imshow("GeeksForGeeks", image)
-
-	# saving image in local storage
-	cv2.imwrite("lane_test.png", image)
-
-	# If keyboard interrupt occurs, destroy image
-	# window
-	cv2.waitKey(0)
-	cv2.destroyWindow("GeeksForGeeks")
-
-# If captured image is corrupted, moving to else part
-else:
-	print("No image detected. Please! try again")
-
 # importing the module
 import cv2
 
