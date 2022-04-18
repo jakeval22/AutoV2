46682b71b8f8add7005e8e06ac7a83aeff6a9b2e
jakeval22 <83443720+jakeval22@users.noreply.github.com>
2022-04-17 22:30:35 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,33 +2,23 @@
 """
 Run inference on images, videos, directories, streams, etc.
 
-Usage - sources:
-    $ python path/to/detect.py --weights yolov5s.pt --source 0              # webcam
-                                                             img.jpg        # image
-                                                             vid.mp4        # video
-                                                             path/          # directory
-                                                             path/*.jpg     # glob
+Usage:
+    $ python path/to/take_img_for_cal.py --weights yolov5s.pt --source 0  # webcam
+                                                             img.jpg  # image
+                                                             vid.mp4  # video
+                                                             path/  # directory
+                                                             path/*.jpg  # glob
                                                              'https://youtu.be/Zgi9g1ksQHc'  # YouTube
                                                              'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP stream
-
-Usage - formats:
-    $ python path/to/detect.py --weights yolov5s.pt                 # PyTorch
-                                         yolov5s.torchscript        # TorchScript
-                                         yolov5s.onnx               # ONNX Runtime or OpenCV DNN with --dnn
-                                         yolov5s.xml                # OpenVINO
-                                         yolov5s.engine             # TensorRT
-                                         yolov5s.mlmodel            # CoreML (macOS-only)
-                                         yolov5s_saved_model        # TensorFlow SavedModel
-                                         yolov5s.pb                 # TensorFlow GraphDef
-                                         yolov5s.tflite             # TensorFlow Lite
-                                         yolov5s_edgetpu.tflite     # TensorFlow Edge TPU
 """
 
 import argparse
 import os
 import sys
+import numpy as np
 from pathlib import Path
 
+import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
@@ -40,18 +30,128 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import DetectMultiBackend
 from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
-from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
 
 
+def roi_transform(image,ul,ur,ll,lr):
+    point_matrix = np.float32([ul, ur, ll, lr])
+    width = image.shape[0]
+    height = image.shape[1]
+    new_ul = [0, 0]
+    new_ur = [width, 0]
+    new_ll = [0, height]
+    new_lr = [width, height]
+    converted_points = np.float32([new_ul,new_ur,new_ll,new_lr])
+    perspective_transform = cv2.getPerspectiveTransform(point_matrix, converted_points)
+    img_Output = cv2.warpPerspective(image, perspective_transform, (width, height))
+    return img_Output
+
+def pipeline(image):
+    cv2.imshow('Original image',image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    edge = cv2.Canny(blur, 10, 50)
+    cv2.imshow('edge', edge)
+    roi_img = roi_transform(edge, [190,140], [450,140], [0,480], [640,480])
+    lane_detx = roi_transform(image, [190,140], [450,140], [0,480], [640,480])
+    lane_dety = np.copy(lane_detx)
+    lane_det = np.copy(lane_detx)
+    cv2.imshow('ROI', roi_img)
+    lines = cv2.HoughLinesP(
+        roi_img,  # Input edge image
+        1,  # Distance resolution in pixels
+        np.pi / 180,  # Angle resolution in radians
+        threshold=100,  # Min number of votes for valid line
+        minLineLength= 30,  # Min allowed length of line
+        maxLineGap= 500  # Max allowed gap between line for joining them
+    )
+    xmax = 240;
+    xmin = 240;
+    x_lines_max = []
+    x_lines_min = []
+    x_lines = []
+    # Iterate over points
+    for points in lines:
+        x1, y1, x2, y2 = points[0]
+        if abs(y1 - y2) > 50:
+            if x1 > 20 or x1 < 20:
+                if x1 > xmax:
+                    xmax = x1
+                    x_lines_max = ([x1,y1,x2,y2])
+                elif x1 < xmin:
+                    xmin = x1
+                    x_lines_min = ([x1,y1,x2,y2])
+    if x_lines_max:
+        x_lines.append(x_lines_max)
+    if x_lines_min:
+        x_lines.append(x_lines_min)
+    for i in range(len(x_lines)):
+        # Extracted points nested in the list
+        x1 = x_lines[i][0]
+        y1 = x_lines[i][1]
+        x2 = x_lines[i][2]
+        y2 = x_lines[i][3]
+        # Draw the lines joing the points
+        # On the original image
+        cv2.line(lane_detx, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    cv2.imshow("lane detx", lane_detx)
+
+    ymax = 320
+    ymin = 320
+    y_lines_max =[]
+    y_lines_min =[]
+    y_lines = []
+    for points in lines:
+        x1, y1, x2, y2 = points[0]
+        if abs(y1 - y2) < 20:
+            if y1 > 20:
+                if y1 > ymax:
+                    ymax = y1
+                    y_lines_max = ([x1,y1,x2,y2])
+                elif y1 < ymin:
+                    ymin = y1
+                    y_lines_min = ([x1,y1,x2,y2])
+    if y_lines_max:
+        y_lines.append(y_lines_max)
+    if y_lines_min:
+        y_lines.append(y_lines_min)
+    for i in range(len(y_lines)):
+        # Extracted points nested in the list
+        x1 = y_lines[i][0]
+        y1 = y_lines[i][1]
+        x2 = y_lines[i][2]
+        y2 = y_lines[i][3]
+        # Draw the lines joing the points
+        # On the original image
+        cv2.line(lane_dety, (x1, y1), (x2, y2), (255, 255, 0), 2)
+
+    cv2.imshow("lane dety", lane_dety)
+    if len(x_lines) == 2:
+        overlay = lane_det.copy()
+        pts = np.array([[x_lines[0][0], x_lines[0][1]], [x_lines[0][2], x_lines[0][3]], [x_lines[1][0], x_lines[1][1]],
+                        [x_lines[1][2], x_lines[1][3]]], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        overlay=cv2.fillPoly(overlay, pts=[pts], color =(0,255,0))
+        alpha = 0.4  # Transparency factor.
+        # Following line overlays transparent rectangle over the image
+        lane_det = cv2.addWeighted(overlay, alpha, lane_det, 1 - alpha, 0)
+
+    if len(y_lines) == 2:
+        overlay = lane_det.copy()
+        pts = np.array([[y_lines[1][2], y_lines[1][3]], [y_lines[1][0], y_lines[1][1]], [y_lines[0][0], y_lines[0][1]],
+                        [y_lines[0][2], y_lines[0][3]]], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        overlay=cv2.fillPoly(overlay, pts=[pts], color =(255,255,0))
+        lane_det = cv2.addWeighted(overlay, alpha, lane_det, 1 - alpha, 0)
+    cv2.imshow("lane det", lane_det)
+
 @torch.no_grad()
-def run(
-        weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
+def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
-        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
-        imgsz=(640, 640),  # inference size (height, width)
+        imgsz=640,  # inference size (pixels)
         conf_thres=0.25,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
@@ -74,7 +174,7 @@ def run(
         hide_conf=False,  # hide confidences
         half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
-):
+        ):
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
@@ -89,28 +189,33 @@ def run(
 
     # Load model
     device = select_device(device)
-    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
-    stride, names, pt = model.stride, model.names, model.pt
+    model = DetectMultiBackend(weights, device=device, dnn=dnn)
+    stride, names, pt, jit, onnx, engine = model.stride, model.names, model.pt, model.jit, model.onnx, model.engine
     imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Half
+    half &= (pt or engine) and device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
+    if pt:
+        model.model.half() if half else model.model.float()
 
     # Dataloader
     if webcam:
         view_img = check_imshow()
         cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt)
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt and not jit)
         bs = len(dataset)  # batch_size
     else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
+        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt and not jit)
         bs = 1  # batch_size
     vid_path, vid_writer = [None] * bs, [None] * bs
 
     # Run inference
-    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
+    model.warmup(imgsz=(1, 3, *imgsz), half=half)  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
     for path, im, im0s, vid_cap, s in dataset:
         t1 = time_sync()
         im = torch.from_numpy(im).to(device)
-        im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+        im = im.half() if half else im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
             im = im[None]  # expand for batch dim
@@ -146,10 +251,13 @@ def run(
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
             annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+
+            cv2.imshow("da", im0)
+            pipeline(im0)
+
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
-
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
@@ -157,8 +265,9 @@ def run(
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
+                    print((torch.tensor(xyxy).view(1, 4)).view(-1).tolist())
                     if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4))).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
@@ -170,11 +279,15 @@ def run(
                         if save_crop:
                             save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
+            # Print time (inference-only)
+            LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
+
             # Stream results
             im0 = annotator.result()
             if view_img:
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
+
 
             # Save results (image with detections)
             if save_img:
@@ -191,12 +304,9 @@ def run(
                             h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
                         else:  # stream
                             fps, w, h = 30, im0.shape[1], im0.shape[0]
-                        save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
+                            save_path += '.mp4'
                         vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer[i].write(im0)
-
-        # Print time (inference-only)
-        LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
 
     # Print results
     t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
@@ -212,7 +322,6 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
     parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
@@ -238,7 +347,7 @@ def parse_opt():
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    print_args(vars(opt))
+    print_args(FILE.stem, opt)
     return opt
 
 
