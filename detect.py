c9af776ecc87e67b8b47841f4570346241599920
jakeval22 <83443720+jakeval22@users.noreply.github.com>
2022-04-19 23:28:23 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -36,7 +36,16 @@ from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
 
 
-def roi_transform(image,ul,ur,ll,lr):
+npzfile = np.load('calibrationdata.npz')
+mtx = npzfile['mtx']
+dist = npzfile['dist']
+rvecs = npzfile['rvecs']
+tvecs = npzfile['tvecs']
+
+def roi_transform(image, ul, ur, ll, lr):
+    """
+    function for showing region of interest given the 4 coordinates (upper left, upper right, lower left, lower right)
+    """
     point_matrix = np.float32([ul, ur, ll, lr])
     width = image.shape[0]
     height = image.shape[1]
@@ -44,46 +53,64 @@ def roi_transform(image,ul,ur,ll,lr):
     new_ur = [width, 0]
     new_ll = [0, height]
     new_lr = [width, height]
-    converted_points = np.float32([new_ul,new_ur,new_ll,new_lr])
+    converted_points = np.float32([new_ul, new_ur, new_ll, new_lr])
     perspective_transform = cv2.getPerspectiveTransform(point_matrix, converted_points)
     img_Output = cv2.warpPerspective(image, perspective_transform, (width, height))
     return img_Output
 
+
 def pipeline(image):
-    cv2.imshow('Original image',image)
+    """
+    full pipeline for lane detection and outputs
+    """
+
+    cv2.imshow('Original image', image)
+    cv2.imwrite('out_img/org_img.jpg', image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
     edge = cv2.Canny(blur, 10, 50)
     cv2.imshow('edge', edge)
-    roi_img = roi_transform(edge, [190,140], [450,140], [0,480], [640,480])
-    lane_detx = roi_transform(image, [190,140], [450,140], [0,480], [640,480])
+    cv2.imwrite('out_img/edge_img.jpg', edge)
+    roi_img = roi_transform(edge, [159, 154], [481, 154], [0, 480], [640, 480])
+    lane_detx = roi_transform(image, [159, 154], [481, 154], [0, 480], [640, 480])
     lane_dety = np.copy(lane_detx)
     lane_det = np.copy(lane_detx)
     cv2.imshow('ROI', roi_img)
+    cv2.imwrite('out_img/roi_img.jpg', roi_img)
+
+    # creates hough lines (estimates straiht lines from edge image
     lines = cv2.HoughLinesP(
         roi_img,  # Input edge image
         1,  # Distance resolution in pixels
-        np.pi / 180,  # Angle resolution in radians
+        np.pi / 150,  # Angle resolution in radians
         threshold=100,  # Min number of votes for valid line
-        minLineLength= 30,  # Min allowed length of line
-        maxLineGap= 500  # Max allowed gap between line for joining them
+        minLineLength=60,  # Min allowed length of line
+        maxLineGap=400  # Max allowed gap between line for joining them
     )
-    xmax = 240;
-    xmin = 240;
+
+    # get size of roi image
+    w = roi_img.shape[0]
+    h = roi_img.shape[1]
+
+    # begin finding the right vertical and left vertical lane (probably should be called y_lines, oops)
+    xmax = 0
+    xmin = h
     x_lines_max = []
     x_lines_min = []
     x_lines = []
-    # Iterate over points
+    # Iterate over points (list of coordinates) for each line
     for points in lines:
         x1, y1, x2, y2 = points[0]
-        if abs(y1 - y2) > 50:
-            if x1 > 20 or x1 < 20:
-                if x1 > xmax:
-                    xmax = x1
-                    x_lines_max = ([x1,y1,x2,y2])
-                elif x1 < xmin:
-                    xmin = x1
-                    x_lines_min = ([x1,y1,x2,y2])
+        # if the change in x value is too large, then it is not a vertical line and should not go in x_lines
+        if abs(x1 - x2) < 80:
+            # if new line vertical line found further to left or right, that line is what we want to output
+            if x1 > xmax:
+                xmax = x1
+                x_lines_max = ([x1, y1, x2, y2])
+            elif x1 < xmin:
+                xmin = x1
+                x_lines_min = ([x1, y1, x2, y2])
+    # x_lines becomes the two lines we want
     if x_lines_max:
         x_lines.append(x_lines_max)
     if x_lines_min:
@@ -94,26 +121,29 @@ def pipeline(image):
         y1 = x_lines[i][1]
         x2 = x_lines[i][2]
         y2 = x_lines[i][3]
-        # Draw the lines joing the points
-        # On the original image
+        # Draw the lines on image for vertical lane detection
         cv2.line(lane_detx, (x1, y1), (x2, y2), (0, 255, 0), 2)
     cv2.imshow("lane detx", lane_detx)
+    cv2.imwrite('out_img/lane_detx.jpg', lane_detx)
 
-    ymax = 320
-    ymin = 320
-    y_lines_max =[]
-    y_lines_min =[]
+    # follow the above code but for horizontal lines and called y_lines
+    ymax = 0
+    ymin = w
+    y_lines_max = []
+    y_lines_min = []
     y_lines = []
     for points in lines:
         x1, y1, x2, y2 = points[0]
-        if abs(y1 - y2) < 20:
-            if y1 > 20:
+        # if change in y coordinate is larger than 50 than it is not a horizontal line
+        if abs(y1 - y2) < 50:
+            # horizontal line must be detected above the bottom of the frame where camera undistortion artifact is
+            if y1 < 580:
                 if y1 > ymax:
                     ymax = y1
-                    y_lines_max = ([x1,y1,x2,y2])
+                    y_lines_max = ([x1, y1, x2, y2])
                 elif y1 < ymin:
                     ymin = y1
-                    y_lines_min = ([x1,y1,x2,y2])
+                    y_lines_min = ([x1, y1, x2, y2])
     if y_lines_max:
         y_lines.append(y_lines_max)
     if y_lines_min:
@@ -124,29 +154,55 @@ def pipeline(image):
         y1 = y_lines[i][1]
         x2 = y_lines[i][2]
         y2 = y_lines[i][3]
-        # Draw the lines joing the points
-        # On the original image
         cv2.line(lane_dety, (x1, y1), (x2, y2), (255, 255, 0), 2)
-
     cv2.imshow("lane dety", lane_dety)
+    cv2.imwrite('out_img/lane_dety.jpg', lane_dety)
+
+    # if two vertical lines are found then draw a transparent lane detection
     if len(x_lines) == 2:
         overlay = lane_det.copy()
+        # x_oc is the average of the ends of the two horizontal lines
+        x_oc = int((x_lines[0][2] + x_lines[1][2]) / 2)
         pts = np.array([[x_lines[0][0], x_lines[0][1]], [x_lines[0][2], x_lines[0][3]], [x_lines[1][0], x_lines[1][1]],
                         [x_lines[1][2], x_lines[1][3]]], np.int32)
         pts = pts.reshape((-1, 1, 2))
-        overlay=cv2.fillPoly(overlay, pts=[pts], color =(0,255,0))
+        overlay = cv2.fillPoly(overlay, pts=[pts], color=(0, 255, 0))
         alpha = 0.4  # Transparency factor.
         # Following line overlays transparent rectangle over the image
         lane_det = cv2.addWeighted(overlay, alpha, lane_det, 1 - alpha, 0)
+        cv2.line(lane_det, (240, 640), (x_oc, 0), (255, 255, 0), 2)
+        # angle drawn from center to x_oc and outputted for steering
+        angle = np.degrees(np.arctan((320 - x_oc) / 640))
+        print("angle: ", angle)
 
+    # if only one vertical line is detected
+    if len(x_lines) == 1:
+        # the line is on the left side of the frame, want a positive angle of 15 degrees for steering
+        if x_lines[0][0] < 320:
+            angle = 15
+        # otherwise the line is on the right and we want to steer to the left (negative angle)
+        else:
+            angle = -15
+        print("angle: ", angle)
+
+    # if no vertical lines detected then there is no angle information so angle cannot be calculated
+    if len(x_lines) == 0:
+        angle = 0
+        print("angle: ", angle)
+
+    # if there are two horizontal lines then fill in a polygon showing where the horizontal lane is detected
     if len(y_lines) == 2:
         overlay = lane_det.copy()
         pts = np.array([[y_lines[1][2], y_lines[1][3]], [y_lines[1][0], y_lines[1][1]], [y_lines[0][0], y_lines[0][1]],
                         [y_lines[0][2], y_lines[0][3]]], np.int32)
         pts = pts.reshape((-1, 1, 2))
-        overlay=cv2.fillPoly(overlay, pts=[pts], color =(255,255,0))
+        overlay = cv2.fillPoly(overlay, pts=[pts], color=(255, 255, 0))
+        alpha = 0.4
         lane_det = cv2.addWeighted(overlay, alpha, lane_det, 1 - alpha, 0)
+
+    # has filled in polygons of where lanes are detected
     cv2.imshow("lane det", lane_det)
+    cv2.imwrite('out_img/lane_det.jpg', lane_det)
 
 @torch.no_grad()
 def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
@@ -252,8 +308,11 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
             imc = im0.copy() if save_crop else im0  # for save_crop
             annotator = Annotator(im0, line_width=line_thickness, example=str(names))
 
-            cv2.imshow("da", im0)
-            pipeline(im0)
+            h, w = im0.shape[:2]
+            newCameraMtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+            undistortedImg = cv2.undistort(im0, mtx, dist, None, newCameraMtx)
+            cv2.imshow("lane detection", undistortedImg)
+            pipeline(undistortedImg)
 
             if len(det):
                 # Rescale boxes from img_size to im0 size
@@ -265,6 +324,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
+                    print((torch.tensor(cls).view(1, 1)).view(-1).tolist())
                     print((torch.tensor(xyxy).view(1, 4)).view(-1).tolist())
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4))).view(-1).tolist()  # normalized xywh
